25fcdd2968e1253031894b88ff7ce0d4eb809746
pawan <pawanpushkar7070@gmail.com>
2026-03-09 08:35:34 -0800

    Initial Android project setup

---
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.firstandroidapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text("Hello Android!")
+}
